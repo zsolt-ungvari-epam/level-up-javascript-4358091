437489d00abde8e941a3996e6ad51a88f41a419a
zsolt-ungvari-epam <50698628+zsolt-ungvari-epam@users.noreply.github.com>
2024-07-08 12:49:52 +0000

    01_08

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,18 @@
-// Write your code here
+function getStudents(classroom) {
+  let { hasTeachingAssistant, classList } = classroom;
+  let teacher, teachingAssinstant, students;
+
+  if (hasTeachingAssistant) {
+    [teacher, teachingAssinstant, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+
+console.log(
+  getStudents({
+    hasTeachingAssistant: false,
+    classList: ["Mr. Garrison", "Mr. Hat", "Kyle", "Eric", "Stan", "Kenny"],
+  })
+);
