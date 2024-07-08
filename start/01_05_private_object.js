6610a9d4a2924880f6b4cb78de43896c4e8ab956
zsolt-ungvari-epam <50698628+zsolt-ungvari-epam@users.noreply.github.com>
2024-07-08 12:00:07 +0000

    01_06

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,11 @@
-// Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username]: "Ungvári Zsolt",
+  [password]: "pswd123",
+  age: 18,
+};
+
+console.log(user.username);
+console.log(user.password);
